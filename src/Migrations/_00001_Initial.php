a71d9635e4ebeb8a6a65ab5eb7bc88846009c59a
Vasileios Pasialiokis <whiterocket@outlook.com>
2023-12-28 15:26:07 +0200

    use exec

--- src/Migrations/_00001_Initial.php
+++ src/Migrations/_00001_Initial.php
@@ -7,14 +7,14 @@ use Lib\Database\IMigration;
 
 class _00001_Initial implements IMigration {
 	public static function run(IDatabase $db): void {
-		$db->query('create table boards (
+		$db->exec('create table boards (
 			id integer primary key autoincrement,
 			handle text unique not null,
 			title text not null,
 			description text not null
 		)');
 
-		$db->query('create table posts (
+		$db->exec('create table posts (
 			id integer primary key autoincrement,
 			thread integer,
 			board integer,
@@ -23,7 +23,7 @@ class _00001_Initial implements IMigration {
 			created_at integer not null
 		)');
 
-		$db->query('create table attachments (
+		$db->exec('create table attachments (
 			post integer primary key,
 			name text not null,
 			contents blob not null,
