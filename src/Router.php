79dd33fa3858150fd4ace0f40459e8577dee1dee
Vasileios Pasialiokis <whiterocket@outlook.com>
2023-12-28 14:41:33 +0200

    routes for attachments and thumbnails

--- src/Router.php
+++ src/Router.php
@@ -1,6 +1,7 @@
 <?php
 namespace App;
 
+use App\Controllers\AttachmentController;
 use App\Controllers\IndexController;
 use App\Controllers\StaticController;
 use Lib\Http\Router as BaseRouter;
@@ -10,11 +11,14 @@ class Router extends BaseRouter {
 	public function __construct(
 		IndexController $indexController,
 		StaticController $staticController,
+		AttachmentController $attachmentController,
 	) {
 		parent::__construct();
 
 		$this
 			->get('/', [$indexController, 'index'])
-			->get('/favicon.ico', [$staticController, 'serve']);
+			->get('/favicon.ico', [$staticController, 'serve'])
+			->get('/attachments/*', [$attachmentController, 'getAttachment'])
+			->get('/thumbnails/*', [$attachmentController, 'getThumbnail']);
 	}
 }
